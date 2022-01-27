76b34af71e4f3d7e8e35051c1e78c9e7fba5c8a5
Anwar Jahid <ajr.jahid@gmail.com>
2022-01-27 16:41:46 +0600

    form done

--- pages/apply/index.js
+++ pages/apply/index.js
@@ -6,6 +6,9 @@ import useSWR from "swr";
 function Apply({districts}) {
     const [ selectValue, setSelectValue ] = useState("")
     const [searchDistrict, setSearchDistrict] = useState([]);
+
+    const [formData, setFormData] = useState([])
+
     const currentStudyingLevel = useRef(null);
     const districtRef = useRef(null);
     const districtInput = useRef(null);
@@ -48,6 +51,26 @@ function Apply({districts}) {
         districtInput.current.value = e.target.innerHTML
     }
 
+    const onClickSameAsPermanent = (e) => {
+        if (e.target.checked){
+            presentAddress.current.setAttribute('disabled', true)
+            presentAddress.current.classList.remove('bg-gray-200');
+            presentAddress.current.classList.add('bg-gray-600');
+
+
+        }else{
+            presentAddress.current.removeAttribute('disabled', true)
+            presentAddress.current.classList.remove('bg-gray-600');
+            presentAddress.current.classList.add('bg-gray-200');
+        }
+    }
+
+    const onFormChange = (e) => {
+        setFormData({
+            [e.target.name] : e.target.value
+        })
+    }
+
 
     let { data, error } = useSWR('/api/major', url => fetch(url).then(r => r.json()))
     if (error) console.log(error)
@@ -67,25 +90,25 @@ function Apply({districts}) {
                         <div className="-mx-3 md:flex mb-6">
                             <div className="md:w-1/2 px-3 mb-6 md:mb-0">
                                 <label htmlFor="name" className="uppercase tracking-wide text-black text-xs font-bold mb-2">Candidate Name<sup className="text-sm text-red-500">*</sup></label>
-                                <input type="text" id="name" required className="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3"/>
+                                <input onChange={onFormChange} type="text" id="name" required className="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3"/>
                             </div>
                             <div className="md:w-1/2 px-3 mb-6 md:mb-0">
                                 <label htmlFor="father_name" className="uppercase tracking-wide text-black text-xs font-bold mb-2">Father's Name<sup className="text-sm text-red-500">*</sup></label>
-                                <input type="text" id="father_name" required className="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3"/>
+                                <input onChange={onFormChange} type="text" id="father_name" required className="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3"/>
                             </div>
                             <div className="md:w-1/2 px-3 mb-6 md:mb-0">
                                 <label htmlFor="mother_name" className="uppercase tracking-wide text-black text-xs font-bold mb-2">Mother's Name<sup className="text-sm text-red-500">*</sup></label>
-                                <input type="text" id="mother_name" required className="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3"/>
+                                <input onChange={onFormChange} type="text" id="mother_name" required className="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3"/>
                             </div>
                         </div>
                         <div className="-mx-3 md:flex mb-6">
                             <div className="md:w-1/2 px-3 mb-6 md:mb-0">
                                 <label htmlFor="dob" className="uppercase tracking-wide text-black text-xs font-bold mb-2">Date of Birth<sup className="text-sm text-red-500">*</sup></label>
-                                <input type="date" required id="dob" className="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3"/>
+                                <input onChange={onFormChange} type="date" required id="dob" className="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3"/>
                             </div>
                             <div className="md:w-1/2 px-3 mb-6 md:mb-0">
                                 <label htmlFor="gender" className="uppercase tracking-wide text-black text-xs font-bold mb-2">Gender<sup className="text-sm text-red-500">*</sup></label>
-                                <select name="gender" required id="gender" className="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3">
+                                <select name="gender" required id="gender" onChange={onFormChange} className="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3">
                                     <option value="male">Male</option>
                                     <option value="female">Female</option>
                                     <option value="other">Other</option>
@@ -93,7 +116,7 @@ function Apply({districts}) {
                             </div>
                             <div className="md:w-1/2 px-3 mb-6 md:mb-0">
                                 <label htmlFor="religion" className="uppercase tracking-wide text-black text-xs font-bold mb-2">Religion<sup className="text-sm text-red-500">*</sup></label>
-                                <select name="religion" required id="religion" className="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3">
+                                <select name="religion" required id="religion" onChange={onFormChange} className="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3">
                                     <option value="islam">Islam</option>
                                     <option value="other">Other</option>
                                 </select>
@@ -102,18 +125,18 @@ function Apply({districts}) {
                         <div className="-mx-3 md:flex">
                             <div className="md:w-1/2 px-3 mb-6 md:mb-0">
                                 <label htmlFor="marital_status" className="uppercase tracking-wide text-black text-xs font-bold mb-2">Marital Status<sup className="text-sm text-red-500">*</sup></label>
-                                <select name="marital_status" required id="marital_status" className="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3">
+                                <select name="marital_status" onChange={onFormChange} required id="marital_status" className="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3">
                                     <option value="single">Single</option>
                                     <option value="married">Married</option>
                                 </select>
                             </div>
                             <div className="md:w-1/2 px-3 mb-6 md:mb-0">
                                 <label htmlFor="nid" className="uppercase tracking-wide text-black text-xs font-bold mb-2">NID</label>
-                                <input type="number" id="nid" className="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3"/>
+                                <input type="number" onChange={onFormChange} id="nid" name="nid" className="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3"/>
                             </div>
                             <div className="md:w-1/2 px-3 mb-6 md:mb-0">
                                 <label htmlFor="photo" className="uppercase tracking-wide text-black text-xs font-bold mb-2">Photo<sup className="text-sm text-red-500">*</sup></label>
-                                <input type="file" accept="image/*" id="photo" className="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3"/>
+                                <input onChange={onFormChange} type="file" accept="image/*" id="photo" className="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3"/>
                             </div>
                         </div>
                     </fieldset>
@@ -123,7 +146,7 @@ function Apply({districts}) {
                         <div className="-mx-3 md:flex mb-6">
                             <div className="md:w-1/2 px-3 mb-6 md:mb-0">
                                 <label htmlFor="last_degree" className="uppercase tracking-wide text-black text-xs font-bold mb-2">Highest Educational Qualification<sup className="text-sm text-red-500">*</sup></label>
-                                <select name="education" required id="last_degree" className="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3">
+                                <select onChange={onFormChange} name="education" required id="last_degree" className="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3">
                                     <option value="VIII">VIII</option>
                                     <option value="JSC">JSC</option>
                                     <option value="JDC">JDC</option>
@@ -143,7 +166,7 @@ function Apply({districts}) {
                             </div>
                             <div className="md:w-1/2 px-3 mb-6 md:mb-0">
                                 <label htmlFor="roll" className="uppercase tracking-wide text-black text-xs font-bold mb-2">Roll Number<sup className="text-sm text-red-500">*</sup></label>
-                                <input type="number" name="roll" required id="roll" className="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3"/>
+                                <input onChange={onFormChange} type="number" name="roll" required id="roll" className="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3"/>
                             </div>
                             <div className="md:w-1/2 px-3 mb-6 md:mb-0">
                                 <label htmlFor="passing_year" className="uppercase tracking-wide text-black text-xs font-bold mb-2">Passing Year<sup className="text-sm text-red-500">*</sup></label>
@@ -151,7 +174,7 @@ function Apply({districts}) {
                             </div>
                             <div className="md:w-1/2 px-3 mb-6 md:mb-0">
                                 <label htmlFor="gpa" className="uppercase tracking-wide text-black text-xs font-bold mb-2">GPA/Marks<sup className="text-sm text-red-500">*</sup></label>
-                                <input type="text" name="gpa" required id="passing_year" className="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3" />
+                                <input onChange={onFormChange} type="text" name="gpa" required id="passing_year" className="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3" />
                             </div>
                         </div>
                     </fieldset>
@@ -170,7 +193,7 @@ function Apply({districts}) {
                             </div>
                             <div className="md:w-1/2 px-3 mb-6 md:mb-0">
                                 <label htmlFor="studying_level" className="uppercase tracking-wide text-black text-xs font-bold mb-2">Present Studying Level<sup className="text-sm text-red-500">*</sup></label>
-                                <select ref={currentStudyingLevel} disabled={true} name="studying_level" required id="studying_level" className="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3">
+                                <select ref={currentStudyingLevel} disabled={true} onChange={onFormChange} name="studying_level" required id="studying_level" className="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3">
                                     <option value="VIII">VIII</option>
                                     <option value="JSC">JSC</option>
                                     <option value="JDC">JDC</option>
@@ -195,15 +218,15 @@ function Apply({districts}) {
                         <div className="-mx-3 md:flex mb-6">
                             <div className="md:w-1/2 px-3 mb-6 md:mb-0">
                                 <label htmlFor="email" className="uppercase tracking-wide text-black text-xs font-bold mb-2">Email</label>
-                                <input type="email" id="email" className="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3"/>
+                                <input type="email"  onChange={onFormChange} name='email' id="email" className="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3"/>
                             </div>
                             <div className="md:w-1/2 px-3 mb-6 md:mb-0">
                                 <label htmlFor="mobile" className="uppercase tracking-wide text-black text-xs font-bold mb-2">Mobile Number<sup className="text-sm text-red-500">*</sup></label>
-                                <input type="tel" name="mobile" required id="mobile" className="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3" />
+                                <input type="tel" onChange={onFormChange} name="mobile" required id="mobile" className="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3" />
                             </div>
                             <div className="md:w-1/2 px-3 mb-6 md:mb-0">
                                 <label htmlFor="guardian_number" className="uppercase tracking-wide text-black text-xs font-bold mb-2">Guardian Number<sup className="text-sm text-red-500">*</sup></label>
-                                <input type="tel" name="guardian_number" required id="guardian_number" className="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3" />
+                                <input type="tel" onChange={onFormChange} name="guardian_number" required id="guardian_number" className="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3" />
                             </div>
                         </div>
                     </fieldset>
@@ -213,11 +236,11 @@ function Apply({districts}) {
                         <div className="-mx-3 md:flex mb-6">
                             <div className="md:w-1/2 px-3 mb-6 md:mb-0">
                                 <label htmlFor="permanent_address" className="uppercase tracking-wide text-black text-xs font-bold mb-2">Permanent Address<sup className="text-sm text-red-500">*</sup></label>
-                                <input className='w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3' name="permanent_address" id='permanent_address' />
+                                <input className='w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3' onChange={onFormChange} name="permanent_address" id='permanent_address' />
                             </div>
                             <div className="md:w-1/2 px-3 mb-6 md:mb-0">
                                 <label htmlFor="district" className="uppercase tracking-wide text-black text-xs font-bold mb-2">District<sup className="text-sm text-red-500">*</sup></label>
-                                <input name="district" required id="district" className="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-1 relative" ref={districtInput} onKeyUp={onDistrictSearch} />
+                                <input name="district" required id="district" onChange={onFormChange} className="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-1 relative" ref={districtInput} onKeyUp={onDistrictSearch} />
                                 <ul className="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-2 mb-3 list-none min-h-50 max-h-40 transition ease-in-out overflow-x-scroll hidden" ref={districtRef}>
                                     {
                                         searchDistrict.map((d, index) => {
@@ -236,12 +259,12 @@ function Apply({districts}) {
                         <div className="-mx-3 md:flex mb-6">
                             <div className="md:w-full px-3 mb-6 md:mb-0">
                                 <label htmlFor="present_address" className="uppercase tracking-wide text-black text-xs font-bold mb-2">Present Address<sup className="text-sm text-red-500">*</sup></label>
-                                <label htmlFor="same_as_permanent" className="text-xs border-2 p-1 border-cyan-200">Same as Permanent <input type="checkbox" ref={same_as_permanent} id="same_as_permanent" className="rounde"/></label>
-                                <textarea ref={presentAddress} className='w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3' name="present_address" id='present_address'></textarea>
+                                <label htmlFor="same_as_permanent" className="text-xs border-2 p-1 border-cyan-200" onClick={onClickSameAsPermanent}>Same as Permanent <input type="checkbox" ref={same_as_permanent} id="same_as_permanent" className="rounde"/></label>
+                                <textarea ref={presentAddress} onChange={onFormChange} className='w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3' name="present_address" id='present_address'></textarea>
                             </div>
                             <div className="md:w-full px-3 mb-6 md:mb-0">
                                 <label htmlFor="reference" className="tracking-wide text-black text-xs font-bold mb-2"><span className='uppercase'>Reference</span><sup className="text-xs font-normal">(if any)</sup></label>
-                                <textarea className='w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3' name="reference" id='reference' placeholder="Name, Designation, Contact Number, Address"></textarea>
+                                <textarea onChange={onFormChange} className='w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3' name="reference" id='reference' placeholder="Name, Designation, Contact Number, Address"></textarea>
                             </div>
                         </div>
                     </fieldset>
